5b471ae7431439f372f0a7f4d4dd1ccd27f28858
Felix <felixnatanael504@gmail.com>
2023-03-21 11:44:26 +0700

    Initial Commit

---
+++ NanoChallenge1/NanoChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge1App.swift
+//  NanoChallenge1
+//
+//  Created by Felix Natanael on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
